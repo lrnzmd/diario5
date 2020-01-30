28da94d77fe021643c530bc79573df17aa2a322e
lrnzm <lrnzmg@gmail.com>
2020-01-30 17:07:32 +0100

    aggiunto collegamento diario google

--- app/src/main/java/com/example/diario5/MainActivity.kt
+++ app/src/main/java/com/example/diario5/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.diario5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.diario5.appunti.Appunti
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, Appunti::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+    fun AddCalendarEvent(view: View?) {
+        val calendarEvent: Calendar = Calendar.getInstance()
+        val i = Intent(Intent.ACTION_EDIT)
+        i.type = "vnd.android.cursor.item/event"
+        i.putExtra("beginTime", calendarEvent.getTimeInMillis())
+        i.putExtra("allDay", true)
+        i.putExtra("rule", "FREQ=YEARLY")
+        i.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000)
+        i.putExtra("title", "Calendar Event")
+        startActivity(i)
     }
 }
---
+++ app/src/main/java/com/example/diario5/appunti/Appunti.kt
@@ -0,0 +1,166 @@
+package com.example.diario5.appunti
+
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diario5.R
+import kotlinx.android.synthetic.main.appunti.*
+import kotlinx.android.synthetic.main.row.view.*
+
+class Appunti : AppCompatActivity() {
+
+    var listNotes = ArrayList<Note>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.appunti)
+
+        //Load from DB
+        LoadQuery("%")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LoadQuery("%")
+    }
+
+    private fun LoadQuery(title: String) {
+        var dbManager = DbManager(this)
+        val projections = arrayOf("ID", "Title", "Description")
+        val selectionArgs = arrayOf(title)
+        val cursor = dbManager.Query(projections, "Title like ?", selectionArgs, "Title")
+        listNotes.clear()
+        if (cursor.moveToFirst()) {
+
+            do {
+                val ID = cursor.getInt(cursor.getColumnIndex("ID"))
+                val Title = cursor.getString(cursor.getColumnIndex("Title"))
+                val Description = cursor.getString(cursor.getColumnIndex("Description"))
+
+                listNotes.add(Note(ID, Title, Description))
+
+            } while (cursor.moveToNext())
+        }
+
+        //adapter
+        var myNotesAdapter = MyNotesAdapter(this, listNotes)
+        //set adapter
+        notesLv.adapter = myNotesAdapter
+
+        //get total number of tasks from ListView
+        val total = notesLv.count
+        //actionbar
+        val mActionBar = supportActionBar
+        if (mActionBar != null) {
+            //set to actionbar as subtitle of actionbar
+            mActionBar.subtitle = "You have $total note(s) in list..."
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            when (item.itemId) {
+                R.id.addNote -> {
+                    startActivity(Intent(this, AddNoteActivity::class.java))
+                }
+                R.id.action_settings -> {
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    inner class MyNotesAdapter : BaseAdapter {
+        var listNotesAdapter = ArrayList<Note>()
+        var context: Context? = null
+
+        constructor(context: Context, listNotesAdapter: ArrayList<Note>) : super() {
+            this.listNotesAdapter = listNotesAdapter
+            this.context = context
+        }
+
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            //inflate layout row.xml
+            var myView = layoutInflater.inflate(R.layout.row, null)
+            val myNote = listNotesAdapter[position]
+            myView.titleTv.text = myNote.nodeName
+            myView.descTv.text = myNote.nodeDes
+            //delete button click
+            myView.deleteBtn.setOnClickListener {
+                var dbManager = DbManager(this.context!!)
+                val selectionArgs = arrayOf(myNote.nodeID.toString())
+                dbManager.delete("ID=?", selectionArgs)
+                LoadQuery("%")
+            }
+            //edit//update button click
+            myView.editBtn.setOnClickListener {
+                GoToUpdateFun(myNote)
+            }
+            //copy btn click
+            myView.copyBtn.setOnClickListener {
+                //get title
+                val title = myView.titleTv.text.toString()
+                //get description
+                val desc = myView.descTv.text.toString()
+                //concatinate
+                val s = title + "\n" + desc
+                val cb = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                cb.text = s // add to clipboard
+                Toast.makeText(this@Appunti, "Copied...", Toast.LENGTH_SHORT).show()
+            }
+            //share btn click
+            myView.shareBtn.setOnClickListener {
+                //get title
+                val title = myView.titleTv.text.toString()
+                //get description
+                val desc = myView.descTv.text.toString()
+                //concatenate
+                val s = title + "\n" + desc
+                //share intent
+                val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, s)
+                startActivity(Intent.createChooser(shareIntent, s))
+            }
+
+            return myView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listNotesAdapter[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listNotesAdapter.size
+        }
+
+    }
+
+    private fun GoToUpdateFun(myNote: Note) {
+        var intent = Intent(this, AddNoteActivity::class.java)
+        intent.putExtra("ID", myNote.nodeID) //put id
+        intent.putExtra("name", myNote.nodeName) //ut name
+        intent.putExtra("des", myNote.nodeDes) //put description
+        startActivity(intent) //start activity
+    }
+}
